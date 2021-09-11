592775044a48269d18ea8f5fcd613a221c811bb1
Felipe de Almeida <felipe.almeida@thinkam.net>
2021-09-11 00:14:15 -0300

    Adicionamos o estilo e fizemos o menu

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
