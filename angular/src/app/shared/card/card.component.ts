69759b73edaa7318129ec8fbb46e11f0b464be91
caiogomesdev <caiogame100@gmail.com>
2022-08-04 19:51:29 -0300

    feat: #6 - add dynamic card

--- angular/src/app/shared/card/card.component.ts
+++ angular/src/app/shared/card/card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CardComponent {
+  @Input() name = '';
+  @Input() place = '';
+  @Input() image = '';
+  @Input() stars = '0';
+  @Input() text = '';
 }
