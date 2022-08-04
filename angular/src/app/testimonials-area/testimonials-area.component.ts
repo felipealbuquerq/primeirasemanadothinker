4060eaed83e3988c04c300d38cd7fb686672f7c7
caiogomesdev <caiogame100@gmail.com>
2022-08-04 19:53:29 -0300

    feat: #6 - configure card dynamic on testimonials

--- angular/src/app/testimonials-area/testimonials-area.component.ts
+++ angular/src/app/testimonials-area/testimonials-area.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ITestimonial } from './models';
 
 @Component({
   selector: 'app-testimonials-area',
   templateUrl: './testimonials-area.component.html',
-  styleUrls: ['./testimonials-area.component.scss']
+  styleUrls: ['./testimonials-area.component.scss'],
 })
-export class TestimonialsAreaComponent implements OnInit {
+export class TestimonialsAreaComponent {
+  testimonialIndex = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  testimonials: ITestimonial[] = [
+    {
+      name: 'Warner Batista',
+      place: 'Presidente prudente, SP',
+      image:
+        'https://files.tecnoblog.net/wp-content/uploads/2022/03/copiar-texto-imagem-2.png',
+      stars: '4,5',
+      text: '"Foi muito bacana .... Quando eu comecei tinha muito medo de pegar algum projeto para fazer. hoje eu já sinto mais confiança.Agora apareceu uma oportunidade bem bacana que antes eu não teria condições de encarar".',
+    },
+  ];
 }
