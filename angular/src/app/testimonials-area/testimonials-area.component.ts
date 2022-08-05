f55307e7d9a8f0794ec2577050767a4d2c317527
caiogomesdev <caiogame100@gmail.com>
2022-08-04 21:15:42 -0300

    feat: #6 - moved card on click button

--- angular/src/app/testimonials-area/testimonials-area.component.ts
+++ angular/src/app/testimonials-area/testimonials-area.component.ts
@@ -35,4 +35,30 @@ export class TestimonialsAreaComponent {
       text: '"Foi muito bacana .... Quando eu comecei tinha muito medo de pegar algum projeto para fazer. hoje eu já sinto mais confiança.Agora apareceu uma oportunidade bem bacana que antes eu não teria condições de encarar".',
     },
   ];
+
+  handleNavigation(value: number, type: 'exact' | 'default' = 'default') {
+    if (type === 'exact') {
+      this.testimonialIndex = value;
+      this.moveCard();
+      return;
+    }
+    const result = Math.min(
+      Math.max(this.testimonialIndex + value, 0),
+      this.testimonials.length - 1
+    );
+    console.log(result);
+
+    this.testimonialIndex = result;
+    this.moveCard();
+  }
+
+  moveCard() {
+    const card = document.querySelector('#card-item') as HTMLElement;
+    const cartMove = document.querySelector('.card-items') as HTMLElement;
+
+    const marginRight = parseInt(getComputedStyle(card).marginRight);
+    const width = card.offsetWidth;
+    const offSet = this.testimonialIndex * (marginRight + width);
+    cartMove.style.transform = `translateX(-${offSet}px)`;
+  }
 }
