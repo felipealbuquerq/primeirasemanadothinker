d00a5b5b91ac5916bca4b99a29427a189b88b9c9
caiogomesdev <caiogame100@gmail.com>
2022-08-05 13:32:08 -0300

    fix: #6 - infinite navigation and height 100%

--- angular/src/app/testimonials-area/testimonials-area.component.ts
+++ angular/src/app/testimonials-area/testimonials-area.component.ts
@@ -31,6 +31,27 @@ export class TestimonialsAreaComponent {
       stars: '4.5',
       text: '"Eu sentia muito a sindrome do impostor, por mais que fizesse o trabalho eu não imaginava que estava agregando valor o bastante,e hoje consigo enxergar a diferença que consigo fazer em um projeto.".',
     },
+    {
+      name: 'Warner Batista',
+      place: 'Presidente prudente, SP',
+      image: 'https://ca.slack-edge.com/T2CSA6T8F-U01JS2G94MP-c75c400ebfa5-512',
+      stars: '4.5',
+      text: '"Foi muito bacana .... Quando eu comecei tinha muito medo de pegar algum projeto para fazer. hoje eu já sinto mais confiança.Agora apareceu uma oportunidade bem bacana que antes eu não teria condições de encarar".',
+    },
+    {
+      name: 'Camila Marques',
+      place: 'São José dos Campos, SP',
+      image: 'https://i.ibb.co/m092Y1J/image.png',
+      stars: '4.5',
+      text: '"Como eu estava em transição de carreira eu tinha medo de pegar um projeto enorme,e hoje eu vejo que são vários passos,e de pouco a pouco o projeto está pronto".',
+    },
+    {
+      name: 'Érica Sene ',
+      place: 'Presidente prudente, SP',
+      image: 'https://ca.slack-edge.com/T2CSA6T8F-U3A05PRRD-ffd4997aba4c-512',
+      stars: '4.5',
+      text: '"Eu sentia muito a sindrome do impostor, por mais que fizesse o trabalho eu não imaginava que estava agregando valor o bastante,e hoje consigo enxergar a diferença que consigo fazer em um projeto.".',
+    },
   ];
 
   handleNavigation(value: number, type: 'exact' | 'default' = 'default') {
@@ -39,10 +60,11 @@ export class TestimonialsAreaComponent {
       this.moveCard();
       return;
     }
-    const result = Math.min(
-      Math.max(this.testimonialIndex + value, 0),
-      this.testimonials.length - 1
-    );
+    const result =
+      this.testimonialIndex + value >= 0
+        ? (this.testimonialIndex + value) % this.testimonials.length
+        : this.testimonials.length - 1;
+
     this.testimonialIndex = result;
     this.moveCard();
   }
