6f72e49c276b0c335a2581004ee7aec2c806d01e
Henrique Lira <92493868+Henrique-Lira@users.noreply.github.com>
2022-08-08 09:10:45 -0300

    Ajustes

--- angular/src/app/global-network/global-network.component.ts
+++ angular/src/app/global-network/global-network.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-global-network',
   templateUrl: './global-network.component.html',
   styleUrls: ['./global-network.component.scss']
 })
-export class GlobalNetworkComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class GlobalNetworkComponent {
 
 }
