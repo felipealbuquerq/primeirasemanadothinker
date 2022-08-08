642560790a059db50b224cd021e541927e60c317
Henrique Lira <92493868+Henrique-Lira@users.noreply.github.com>
2022-08-07 22:25:34 -0300

    create componet footer

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { ProcessComponent } from './process/process.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ProcessComponent } from './process/process.component';
     MenuComponent,
     HomeComponent,
     CounterComponent,
-    ProcessComponent
+    ProcessComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
