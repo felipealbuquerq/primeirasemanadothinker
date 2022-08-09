32f1c14868915d512c037cf3af3fc5a4aa0a52a1
caiogomesdev <caiogame100@gmail.com>
2022-08-09 19:28:32 -0300

    feat: #7 - add cta-card-footer component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { ProcessComponent } from './process/process.component';
+import { CtaCardFooterComponent } from './cta-card-footer/cta-card-footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ProcessComponent } from './process/process.component';
     MenuComponent,
     HomeComponent,
     CounterComponent,
-    ProcessComponent
+    ProcessComponent,
+    CtaCardFooterComponent
   ],
   imports: [
     BrowserModule,
