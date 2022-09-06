87cad2c8289b5f34fcd29ef847d646e32c9a0dfb
Henrique Lira <92493868+Henrique-Lira@users.noreply.github.com>
2022-09-06 19:39:26 -0300

    Navbar responsiva com botão

--- angular/src/app/menu/menu.component.ts
+++ angular/src/app/menu/menu.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+
+  show: any;
+
   linkSignUp = 'https://airtable.com/shrj7wn8pMzhZ4IwE';
 
   isActive = (route: string): boolean => location.href.includes(route);  
