4395058af5dafe196295fcb4f551c51832b833c2
Felipe A de Almeida <felipe.almeida@thinkam.net>
2022-09-05 23:12:24 -0300

    added firebase

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor() {
+    const firebaseConfig = {
+      apiKey: environment.apiKey,
+      authDomain: environment.authDomain,
+      projectId: environment.projectId,
+      storageBucket: environment.storageBucket,
+      messagingSenderId: environment.messagingSenderId,
+      appId: environment.appId,
+      measurementId: environment.measurementId
+    };
+
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }
 }
